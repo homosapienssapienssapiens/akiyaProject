66c3ad36b0666708285132d11700228caf0363c7
homosapienssapienssapiens <dltkddnl0141@gmail.com>
2023-04-20 14:08:45 +0900

    master

---
+++ src/main/java/com/example/akiyaProject/AkiyaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.akiyaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiyaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkiyaProjectApplication.class, args);
+	}
+
+}
