975b79165c807c802fd93d7844e8b77a0b4c6ecb
github yuztyang@gmail.com <User@DESKTOP-09BQ82O>
2023-04-24 17:13:14 +0900

    1

--- src/main/java/com/project/akiyaProject/controller/RentController.java
+++ src/main/java/com/project/akiyaProject/controller/RentController.java
@@ -7,39 +7,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.project.akiyaProject.model.HouseInfo;
 import com.project.akiyaProject.model.HouseRegForm;
 import com.project.akiyaProject.service.RentService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @RequestMapping("rent")
 @Controller
 public class RentController {
 
-    private final RentService rentService;
+//    private final RentService rentService;
 
-
-<<<<<<< HEAD
     @GetMapping("registration")
     public String registrationForm(Model model) {
     	model.addAttribute("writeForm", new HouseRegForm());
         return "board/write";
     }
 
-//    매물 전체 보기
-    @GetMapping("/list/{house_id}")
-    public ResponseEntity<HouseRegForm> getHouseRegForm(@PathVariable String house_id) {
-        HouseRegForm houseregform = RentService.getHouseById(house_id);
-        if (houseregform == null) {
+
+    
+//    매물 상세 정보 보기
+    @GetMapping("/list/{house_id}")	
+    public ResponseEntity<HouseInfo> getHouseInfo(@PathVariable String house_id) {
+        HouseInfo houseinfo = RentService.getHouseById(house_id);
+        if (houseinfo == null) {
             return ResponseEntity.notFound().build();
         } else {
-            return ResponseEntity.ok(houseregform);
+            return ResponseEntity.ok(houseinfo);
         }
     }
+    
 
-=======
->>>>>>> branch 'master' of https://github.com/homosapienssapienssapiens/akiyaProject.git
 }
--- src/main/java/com/project/akiyaProject/service/RentService.java
+++ src/main/java/com/project/akiyaProject/service/RentService.java
@@ -3,7 +3,9 @@ package com.project.akiyaProject.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.akiyaProject.model.HouseInfo;
 import com.project.akiyaProject.model.HouseRegForm;
+import com.project.akiyaProject.repository.BoardMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class RentService {
 	
-	public static HouseRegForm getHouseById(String house_id) {
-		
-		return null;
+    private final BoardMapper boardMapper;
+	
+	public static HouseInfo getHouseById(String house_id) {
+
+		return BoardMapper.getHouseById(house_id);
 	}
 
 }
