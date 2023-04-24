c3bdb7e6f410d4fd68cc3f546d4ffdad6dfbc21f
homosapienssapienssapiens <dltkddnl0141@gmail.com>
2023-04-24 13:05:25 +0900

    Merge branch 'master' of https://github.com/homosapienssapienssapiens/akiyaProject.git

--- src/main/java/com/project/akiyaProject/controller/RentController.java
+++ src/main/java/com/project/akiyaProject/controller/RentController.java
@@ -13,18 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("board")
+@RequestMapping("rent")
 @Controller
 public class RentController {
 
     private final RentService rentService;
-
-
-    @GetMapping("register")
-    public String registrationForm(Model model) {
-    	model.addAttribute("writeForm", new HouseRegForm());
-        return "houseregister";
-    }
 
 
 }
